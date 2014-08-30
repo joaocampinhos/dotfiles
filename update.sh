4545602a847f1e74ca1df62da174b8e3dcce7fdf
joaocampinhos <joao.campinhos@gmail.com>
2014-08-30 16:33:09 +0100

    fix hybrid.vim specificity

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "update.sh" \
-  --exclude "README.md" --exclude "hybrid.vim" -avh --no-perms . ~;
+  --exclude "README.md" --exclude "./hybrid.vim" -avh --no-perms . ~;
 
 cp hybrid.vim ~/.vim/bundle/vim-airline/autoload/airline/themes;
