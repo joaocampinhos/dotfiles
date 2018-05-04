26e2d26516c90c98f19382ac539cce1bc6527204
João Campinhos <joao.campinhos@feedzai.com>
2018-05-04 14:49:08 +0100

    adds brew script 🍺

---
+++ brew.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+brew update
+
+brew upgrade
+
+brew instal git
+brew instal git-extras
+brew instal grep
+brew instal neovim
+brew instal node
+brew instal ranger
+brew instal stow
+brew instal the_silver_searcher
+brew instal tig
+brew instal tmux
+brew instal zsh
+brew instal zsh-completions
+brew instal zsh-syntax-highlighting
