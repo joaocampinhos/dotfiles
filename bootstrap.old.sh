b6eb3f24f49ec36d65ae612eaa23d7e0b525f963
João Campinhos <joao@campinhos.pt>
2023-12-20 15:27:38 +0000

    did a thing

---
+++ bootstrap.old.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# install brew
+
+brew update
+
+brew upgrade
+
+brew instal git
+brew instal git-extras
+brew instal grep
+brew instal --HEAD neovim
+brew instal node
+brew instal ranger
+brew instal stow
+brew instal the_silver_searcher
+brew instal tig
+brew instal tmux
+brew instal zsh
+brew instal zsh-completions
+brew instal zsh-syntax-highlighting
+brew instal yarn
+brew instal carthage
+brew install thefuck
+brew install gnupg
+brew install rg
+brew install watchman
+
+# pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+sudo pip install pillow
+
+
+brew cask install kitty
+brew cask install google-chrome
+brew cask install react-native-debugger
+brew cask install slack
+brew cask install android-studio
+brew cask install spotify
+brew cask install visual-studio-code
+brew cask install fliqlo
+brew cask install transmission
+
+# pure prompt
+
+# stow stuff
