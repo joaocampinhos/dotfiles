4524e84ffdbda5f1641034806697c0acedbd6180
João Campinhos <joao@campinhos.pt>
2023-12-21 11:06:12 +0000

    found it

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,6 @@ source $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-env -iA nixpkgs.neovim > /tmp/lixo 2>&1
 
 cd $HOME/.dotfiles
+git submodule update --init --recursive
 stow nvim -t $HOME
 stow nvchad-custom -t $HOME/.config/nvim/lua
-
-#cp -a nvim/. ~
-#cp -a nvchad-custom/. ~/.config/nvim/lua
